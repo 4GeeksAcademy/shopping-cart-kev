11af5a2ed809c61c387cbf2ae0d2b8054f29c22d
Octavio Lara <octavioasapchi@gmail.com>
2023-12-06 19:52:38 -0400

    ADD footer component

---
+++ src/js/component/footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+  console.log("Renderizado Footer");
+  return (
+    <footer>
+      <h2>Ecommerce latam-24</h2>
+    </footer>
+  );
+};
+
+export default Footer;
